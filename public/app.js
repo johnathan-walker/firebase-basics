1d4ff136a3126398bc1a6160ff1ae3c3edaccfe1
Johnathan Walker <johnathan.walker23832@gmail.com>
2021-01-16 08:29:44 -0500

    Adding Google Login Provider.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,15 @@
-console.log(firebase)
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById("whenSignedIn");
+const whenSignedOut = document.getElementById("whenSignedOut");
+
+const signInButton = document.getElementById("signInButton");
+const signOutButton = document.getElementById("signOutButton");
+
+const userDetails = document.getElementById("userDetails");
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInButton.onclick = () => auth.signInWithPopup(provider);
+
+signOutButton.onclick = () => auth.signOut();

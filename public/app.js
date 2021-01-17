6b7da1ed2d2bdc84d8cab583632bd0be1a9882b1
Johnathan Walker <johnathan.walker23832@gmail.com>
2021-01-17 07:12:33 -0500

    Listing things store in firestore backend.

--- public/app.js
+++ public/app.js
@@ -1,15 +1,72 @@
+// User Auth
 const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
 const whenSignedIn = document.getElementById("whenSignedIn");
 const whenSignedOut = document.getElementById("whenSignedOut");
-
 const signInButton = document.getElementById("signInButton");
 const signOutButton = document.getElementById("signOutButton");
-
 const userDetails = document.getElementById("userDetails");
 
-const provider = new firebase.auth.GoogleAuthProvider();
-
 signInButton.onclick = () => auth.signInWithPopup(provider);
-
 signOutButton.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+
+    } else {
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
+
+
+// Firestore 
+
+const db = firebase.firestore();
+
+const createThing = document.getElementById('createThing');
+const thingsList = document.getElementById('thingsList');
+
+let thingsRef;
+let unsubscribe;
+
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        thingsRef = db.collection('things');
+
+        createThing.onclick = () =>  {
+
+            const { serverTimestamp } = firebase.firestore.FieldValue;
+
+            thingsRef.add({
+                uid: user.uid,
+                name: faker.commerce.productName(),
+                createdAt: serverTimestamp()
+            });
+        }
+
+        unsubscribe = thingsRef
+            .where('uid' , '==', user.uid)
+            .orderBy('createdAt')
+            .onSnapshot(querySnapshot => {
+                const items = querySnapshot.docs.map(doc => {
+                    return `<li>${doc.data().name}</li>`
+                });
+
+                thingsList.innerHTML = items.join('');
+            });
+    } else {
+        // Unsubscribe when the user signs out
+        unsubscribe && unsubscribe();
+    }
+
+});
+
